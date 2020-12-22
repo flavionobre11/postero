4bef4bb20950f1d80cd744b3f71e7440ecb00fd7
flavionobre11 <flavionobre11@gmail.com>
2020-12-22 02:20:22 -0300

    update

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyC07tXpT73U9GAHIPGfOpSVYTZcOHNMTRA",
+    authDomain: "api-postero.firebaseapp.com",
+    databaseURL: "https://api-postero-default-rtdb.firebaseio.com",
+    projectId: "api-postero",
+    storageBucket: "api-postero.appspot.com",
+    messagingSenderId: "72060355395",
+    appId: "1:72060355395:web:5662f21c120a9266a7a5f6",
+    measurementId: "G-35L2MG4JR1"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
