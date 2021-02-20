6b6712f6da8f30a0e3cdd64830292db5500687ca
flavionobre11 <flavionobre11@gmail.com>
2021-02-20 13:42:08 -0300

    route title update

--- src/router.js
+++ src/router.js
@@ -10,11 +10,16 @@ Vue.use(VueRouter);
 
 const Router = new VueRouter({
     routes: [
-        {path:'*', redirect: '/'},
-        {path:'/', name:'login', component: LoginComponent},
-        {path:'/dash', name:'dash', component:DashComponent},
-        {path:'/clientes', name:'clientes', component: ClienteComponent}
+        {path:'*', redirect: '/', meta: { title: 'Postero' }},
+        {path:'/', name:'login', component: LoginComponent, meta: { title: 'Postero - Login' }},
+        {path:'/dash', name:'dash', component:DashComponent, meta: { title: 'Postero - Dashboard' }},
+        {path:'/clientes', name:'clientes', component: ClienteComponent, meta: { title: 'Postero - Clientes' }}
     ]
 });
+
+Router.beforeEach((to, from, next) => {
+    document.title = to.meta.title
+    next()
+  })
 
 export default Router
